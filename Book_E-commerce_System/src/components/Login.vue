d84722ae84489e5c8340ea8f85e84f131d05ab65
zkd <17301146@bjtu.edu.cn>
2020-07-06 16:27:21 +0800

    第一次提交

---
+++ Book_E-commerce_System/src/components/Login.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="poster">
+    <el-form class="login-container" label-position="left" label-width="0px">
+      <h2 class="login_title">乘风破浪的妹妹们图书商城</h2>
+      <el-form-item>
+        <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item style="width: 100%">
+        <el-button type="danger" plain style="float: left; font-weight:bold; width: 40%" v-on:click="login">注册</el-button>
+        <el-button type="primary" plain  style="float: right; font-weight:bold; width: 40%" v-on:click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        loginForm: {
+          username: '',
+          password: ''
+        },
+        responseResult: []
+      }
+    },
+
+    methods: {
+      login () {
+        this.$axios
+          .post('/login', {
+            username: this.loginForm.username,
+            password: this.loginForm.password
+          })
+          .then(successResponse => {
+            if (successResponse.data.code === 200) {
+              this.$router.replace({path: '/index'})
+            }
+          })
+          .catch(failResponse => {
+          })
+      }
+    }
+  }
+</script>
+
+<style>
+  #poster {
+    background: url("../assets/background.png") no-repeat center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
+  }
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 12% auto 90px;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+
+</style>
+
